e8955f0991cb4da27163b4545113d3170ed517fb
Daniel Usani <daniel.e.usani@gmail.com>
2017-10-17 21:52:57 -0400

    Intro to JS
    Finished intro.js

---
+++ introToJs/js/script.js
@@ -0,0 +1,12 @@
+var user = {
+    name: "Daniel Usani",
+    companyName: "TechLaunch.io",
+    address: "8400 NW 36th #450, Doral, FL 33166",
+    email: "dusani@techlaunch.io",
+    picture: "headshot.jpg"
+}
+
+document.querySelector(".name").textContent = user.name;
+document.querySelector(".company_name").textContent = user.companyName;
+document.querySelector(".address").textContent = user.address;
+document.querySelector(".email").textContent = user.email;
